4146507bd1dec2e79bda19c7248f1cbf5d13768e
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2022-08-27 05:14:39 +0200

    launchScreen & onboarding screen :)

--- AnimeUwU/Views/OnboardingViewController.swift
+++ AnimeUwU/Views/OnboardingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  OnboardingViewController.swift
 //  AnimeUwU
 //
 //  Created by KhaleD HuSsien on 27/08/2022.
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OnboardingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    
 
 }
-
